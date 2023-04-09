41c8e04c31d38217a07b5c21e3405debcdef0508
José Humanes Humanes <1357660+jhhumanes@users.noreply.github.com>
2023-04-09 06:57:03 +0200

    Tutorial.

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -45,4 +45,9 @@ const appMenuItems = <MenuItem>[
     subtitle: 'Una serie de controles de Flutter', 
     link: '/ui-controls', 
     icon: Icons.car_rental_outlined),
+  MenuItem(
+    title: 'Introducción a la aplicación', 
+    subtitle: 'Pequeño tutorial introductorio', 
+    link: '/tutorial', 
+    icon: Icons.accessible_rounded),
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -39,6 +39,11 @@ final appRouter = GoRouter(
       path: '/ui-controls',
       builder: (context, state) => const UiControlsScreen(),
     ),
+    GoRoute(
+      name: AppTutorialScreen.name,
+      path: '/tutorial',
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
   ]
 );
 
